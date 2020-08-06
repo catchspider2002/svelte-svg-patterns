8ff19eeaf72c0a1cd25ca937355d61823a36f049
Naveen <catchspider2002@users.noreply.github.com>
2020-08-06 12:34:26 +0530

    Create Tab.svelte

---
+++ src/components/Tab.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+	registerTab(tab);
+</script>
+
+<style>
+	button {
+		background: none;
+		border: none;
+		border-bottom: 2px solid white;
+		border-radius: 0;
+		margin: 0;
+		color: #ccc;
+		cursor: pointer;
+	}
+	
+	.selected {
+		border-bottom: 2px solid teal;
+		color: yellow;
+	}
+</style>
+
+<button class:selected="{$selectedTab === tab}" on:click="{() => selectTab(tab)}">
+	<slot></slot>
+</button>
