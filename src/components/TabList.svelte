c038d34b41160652dec16dc022d0f15f06645dc8
Naveen <catchspider2002@users.noreply.github.com>
2020-08-06 12:34:07 +0530

    Create TabList.svelte

---
+++ src/components/TabList.svelte
@@ -0,0 +1,16 @@
+<div class="tab-list">
+	<slot></slot>
+</div>
+
+<style>
+	.tab-list {
+		position: fixed;
+		bottom: 0;
+		left:0;
+		border-bottom: 1px solid teal;
+		background-color: black;
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+</style>
