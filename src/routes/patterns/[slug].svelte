335b51dc7cbb6ce2ab1784b7e1b0f5020e9832f4
Naveen <catchspider2002@users.noreply.github.com>
2020-08-10 11:32:27 +0530

    Reverting back the color array

--- src/routes/patterns/[slug].svelte
+++ src/routes/patterns/[slug].svelte
@@ -32,12 +32,12 @@
     for (let i = 1; i <= colorCount; i++) createPicker("color" + i + "Div", "color" + i);
   }
 
-  let svgPattern = (colors, stroke, scale, spacing, angle, join) => {
+  let svgPattern = (color1, color2, stroke, scale, spacing, angle, join) => {
     let strokeFill, joinMode;
     if (mode[0] === "stroke") {
-      strokeFill = " stroke = '" + colors[1] + "' fill='none'";
+      strokeFill = " stroke = '" + color2 + "' fill='none'";
       joinMode = join == 2 ? "stroke-linejoin='round' stroke-linecap='round' " : "stroke-linecap='square' ";
-    } else strokeFill = " stroke = 'none' fill='" + colors[1] + "'";
+    } else strokeFill = " stroke = 'none' fill='" + color2 + "'";
 
     let patternNew =
       "<svg id='testId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs>" +
@@ -50,7 +50,7 @@
       ") rotate(" +
       angle +
       ")'><rect x='0' y='0' width='100%' height='100%' fill='" +
-      colors[0] +
+      color1 +
       "'/><g " +
       "transform='translate(" +
       spacing[0] / 2 +
@@ -108,7 +108,8 @@
   const presetPatterns = [
     {
       id: 1,
-      colors: ["white", "black", "green", "yellow", "red"],
+      color1: "white",
+      color2: "black",
       stroke: 0.5,
       scale: 1,
       spacing: [0, 0],
@@ -117,7 +118,8 @@
     },
     {
       id: 2,
-      colors: [constants.randomColor(0.8), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1)],
+      color1: constants.randomColor(0.8),
+      color2: constants.randomColor(1),
       stroke: constants.randomNumber(0.5, maxStroke),
       scale: constants.randomNumber(1, maxScale / 3),
       spacing: [
@@ -129,7 +131,8 @@
     },
     {
       id: 3,
-      colors: [constants.randomColor(1), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1)],
+      color1: constants.randomColor(1),
+      color2: constants.randomColor(1),
       stroke: constants.randomNumber(0.5, maxStroke),
       scale: constants.randomNumber(1, maxScale / 2),
       spacing: [
@@ -141,7 +144,8 @@
     },
     {
       id: 4,
-      colors: [constants.randomColor(0.9), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1)],
+      color1: constants.randomColor(0.9),
+      color2: constants.randomColor(1),
       stroke: constants.randomNumber(0.5, maxStroke),
       scale: constants.randomNumber(1, maxScale / 2),
       spacing: [
@@ -155,7 +159,8 @@
 
   let selectedPattern = presetPatterns[0];
   $: svgFile = svgPattern(
-    selectedPattern.colors,
+    selectedPattern.color1,
+    selectedPattern.color2,
     selectedPattern.stroke,
     selectedPattern.scale,
     selectedPattern.spacing,
@@ -180,7 +185,8 @@
   function randomPattern() {
     selectedPattern = {
       id: 5,
-      colors: [constants.randomColor(0.8), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1), constants.randomColor(1)],
+      color1: constants.randomColor(0.8),
+      color2: constants.randomColor(1),
       stroke: constants.randomNumber(0.5, maxStroke),
       scale: constants.randomNumber(1, maxScale),
       spacing: [
@@ -409,12 +415,12 @@
     <div class="samples">
       {#each presetPatterns as pattern}
         <button id="pattern{pattern.id}" class="pattern" on:click={check}
-          style={'background-image: url("data:image/svg+xml,' + svgPattern(pattern.colors, pattern.stroke, pattern.scale, pattern.spacing, pattern.angle, pattern.join) + '"' + ')'} />
+          style={'background-image: url("data:image/svg+xml,' + svgPattern(pattern.color1, pattern.color2, pattern.stroke, pattern.scale, pattern.spacing, pattern.angle, pattern.join) + '"' + ')'} />
       {/each}
     </div>
 
     <div class="inputs">
-      <label for="scale">Scale Test</label>
+      <label for="scale">Scale</label>
       <input id="scale" type="range" bind:value={selectedPattern.scale} min="1" max={maxScale} /> 
       <input class="uneditable" bind:value={selectedPattern.scale} readonly/>
       {#if (mode[0] === 'stroke')}
