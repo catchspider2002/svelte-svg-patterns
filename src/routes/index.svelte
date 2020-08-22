95c0295c4d33b084ea5e830c8ab6efbd57cd563d
Naveen <catchspider2002@gmail.com>
2020-08-22 18:00:41 +0530

    Added transparent mockups

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -297,6 +297,41 @@
     /* padding: 2em 0; */
   }
 
+  #cf {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  #cf * {
+    position: absolute;
+  }
+
+  .top {
+    animation-name: fade;
+    animation-timing-function: step-start;
+    animation-iteration-count: infinite;
+    animation-duration: 4s;
+    animation-direction: alternate;
+  }
+
+  @keyframes fade {
+    0% {
+      opacity: 1;
+    }
+    25% {
+      opacity: 1;
+    }
+    75% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  
+
   /*   .header__bg {
     position: absolute;
     width: 100%;
@@ -542,8 +577,19 @@
       <div
         class="sm:w-32 order-first sm:order-none sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-gray-800
         flex-shrink-0">
-        <div class="big-icon">
-          <span class="iconify" data-icon="vaadin:css" data-inline="false" />
+        <div id="cf">
+          <!-- <img class="bottom" src="http://lorempixel.com/200/100/sports/1" /> -->
+          <!-- <img class="top" src="http://lorempixel.com/200/100/sports/2" /> -->
+          <svg class="bottom" viewBox="0 0 340 333">
+  <path class="path" fill="red" stroke="green" stroke-width="4" d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
+    s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
+    C46.039,146.545,53.039,128.545,66.039,133.545z"/>  
+</svg>
+          <svg class="top" viewBox="0 0 340 333">
+  <path class="path" fill="white" stroke="black" stroke-width="4" d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
+    s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
+    C46.039,146.545,53.039,128.545,66.039,133.545z"/>  
+</svg>
         </div>
       </div>
     </div>
@@ -564,7 +610,8 @@
   <img src="coffee-cup-mockup.png" />
   <img src="book-cover-mock-up.png" />
   <img src="flying-pillows-mockup-set.png" />
+  `
   <img src="phone-case-photoshop-mockup.png" />
   <img src="tote-bag-mockup-3d-rendering-design.png" />
-  <img src="flying-pillows-mockup-set.png" />
+  <img src="bussiness-card-mockup.png" />
 </section>
