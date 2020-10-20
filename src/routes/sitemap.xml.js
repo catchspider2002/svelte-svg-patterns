031c8b81df34a9604df9f5dd23671aef055efe02
Naveen <catchspider2002@gmail.com>
2020-10-20 23:45:03 +0530

    Added robots.txt and updated sitemap.xml

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -5,17 +5,19 @@ import patterns from "./patterns/_patterns.js";
 let siteUrl = "https://pattern.monster";
 // siteUrl = "http://localhost:3001";
 
+{/* <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"> */}
+
+
 const renderSitemapXml = contextPaths => `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${contextPaths
   .map(
     path => `
   <url>
     <loc>${siteUrl}/${path}</loc>
     <changefreq>daily</changefreq>
-    <priority>0.7</priority>
-  </url>
-`
+    <priority>1.0</priority>
+  </url>`
   )
   .join("\n")}
 </urlset>`;
