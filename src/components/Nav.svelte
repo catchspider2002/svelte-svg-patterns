f824132c6c70156ec0f983981970cdd8f8029579
Naveen <catchspider2002@gmail.com>
2020-10-24 18:03:23 +0530

    Moved icons to constants file

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,16 +3,15 @@
   import { fly } from "svelte/transition";
   // import { createEventDispatcher } from "svelte";
   import { onMount } from "svelte";
+  import Constants from "../routes/_constants.js";
 
   // const dispatch = createEventDispatcher();
 
   // const triggerEvent = () => {
   //   dispatch("hello", "Rock");
   // };
-
-  let dark = "M12 3c0.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z";
-  let light =
-    "m 3,12 h 1 m 8,-9 v 1 m 8,8 h 1 m -9,8 v 1 M 5.6,5.6 6.3,6.3 M 18.4,5.6 17.7,6.3 m 0,11.4 0.7,0.7 M 6.3,17.7 5.6,18.4 M 16,12 a 4,4 0 0 1 -4,4 4,4 0 0 1 -4,-4 4,4 0 0 1 4,-4 4,4 0 0 1 4,4 z";
+  let dark = Constants.icons.dark;
+  let light = Constants.icons.light;
   let theme = light;
   // let themeColor = "dark";
 
@@ -112,9 +111,9 @@
   }
 
   @media (max-width: 768px) {
-nav{
-    padding: 1em 1em;
-  }
+    nav {
+      padding: 1em 1em;
+    }
 
     .logo {
       justify-self: center;
@@ -195,7 +194,7 @@ nav{
     <a rel="prefetch" target="_blank" href="https://github.com/catchspider2002/svelte-svg-patterns">
       <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path
-          d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+          d={Constants.icons.github} />
       </svg></a>
     <button on:click={() => changeTheme()}>
       <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
--- src/routes/_constants.js
+++ src/routes/_constants.js
@@ -13,4 +13,13 @@ const randomColor = opacity =>
   opacity +
   ")";
 
-export default { randomNumber, randomAngle, randomColor };
+const icons = {
+  github:
+    "M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5",
+  dark: "M12 3c0.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z",
+  light:
+    "m 3,12 h 1 m 8,-9 v 1 m 8,8 h 1 m -9,8 v 1 M 5.6,5.6 6.3,6.3 M 18.4,5.6 17.7,6.3 m 0,11.4 0.7,0.7 M 6.3,17.7 5.6,18.4 M 16,12 a 4,4 0 0 1 -4,4 4,4 0 0 1 -4,-4 4,4 0 0 1 4,-4 4,4 0 0 1 4,4 z",
+  twitter: ""
+};
+
+export default { randomNumber, randomAngle, randomColor, icons };
