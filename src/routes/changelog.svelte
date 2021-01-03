68066041e130f8781f8f30e74aa2a3c49e7e5d50
Naveen <catchspider2002@gmail.com>
2021-01-03 12:51:58 +0530

    Updated changelog and README

--- src/routes/changelog.svelte
+++ src/routes/changelog.svelte
@@ -84,6 +84,7 @@
     <div>
       <ul id="logs">
         <li class="versionHeader">January 2, 2021</li>
+        <li>Added GitHub stars to the navbar icon</li>
         <li>Added 14 new patterns</li>
         <li>Added 6 pattern set to <a title="Downloads" href="downloads">Downloads</a></li>
         <li class="versionHeader">December 26, 2020</li>
