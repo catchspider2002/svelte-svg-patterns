2537a74aeecb8a7a866c5717c7cc8649c1dcf190
Naveen <catchspider2002@gmail.com>
2021-07-26 02:22:09 +0530

    Automated pickr and global css file generation and addition to template.html

--- nodejs/htmlGenerator.js
+++ nodejs/htmlGenerator.js
@@ -3,7 +3,27 @@ const fsextra = require("fs-extra");
 
 const xlsxFile = require("read-excel-file/node");
 
-// let array = { waves: "Wellen", chevron: "Zickzack", "straight lines": "Gerade Linien", lines: "Linien" };
+const minify = require("@node-minify/core");
+const cleanCSS = require("@node-minify/clean-css");
+const glob = require("glob");
+
+let timestamp = Math.round(+new Date() / 1000);
+let globalCSS = "global-" + timestamp + ".min.css";
+let pickrCSS = "pickr-" + timestamp + ".min.css";
+
+minify({
+  compressor: cleanCSS,
+  input: "../src/css/global.css",
+  output: "../src/css/" + globalCSS,
+  callback: function (err, min) {},
+});
+
+minify({
+  compressor: cleanCSS,
+  input: "../src/css/pickr.css",
+  output: "../src/css/" + pickrCSS,
+  callback: function (err, min) {},
+});
 
 xlsxFile("../../../../OneDrive/Documents/Patterns.xlsm", { sheet: "Patterns" }).then((rows) => {
   let languages = [
@@ -16,7 +36,7 @@ xlsxFile("../../../../OneDrive/Documents/Patterns.xlsm", { sheet: "Patterns" }).
     { lang: "ro", name: "Română", col: 7, site: "https://ro.pattern.monster", path: "../../ro.pattern.monster/" },
     { lang: "fr", name: "Français", col: 8, site: "https://fr.pattern.monster", path: "../../fr.pattern.monster/" },
     { lang: "ar", name: "العربية", col: 9, site: "https://ar.pattern.monster", path: "../../ar.pattern.monster/" },
-    
+
     // { lang: "uk", name: "Українська", col: 7, path: "../../uk.pattern.monster/" },
     // { lang: "af", name: "Afrikaans", col: 8, path: "../../af.pattern.monster/" },
   ];
@@ -24,12 +44,10 @@ xlsxFile("../../../../OneDrive/Documents/Patterns.xlsm", { sheet: "Patterns" }).
   languages.forEach((language) => {
     xlsxFile("../../../../OneDrive/Documents/Patterns.xlsm", { sheet: "Translation" }).then((transRows) => {
       let array = getArray(language.col);
-      // console.log(array);
 
       function getArray(col) {
         let o = {};
         for (let j = 63; j < transRows.length; j++) {
-          // for (let j = 1; j < 4; j++) {
           o[transRows[j][1].toLowerCase()] = transRows[j][col] === "" || !transRows[j][col] ? transRows[j][1] : transRows[j][col];
         }
 
@@ -44,6 +62,81 @@ xlsxFile("../../../../OneDrive/Documents/Patterns.xlsm", { sheet: "Patterns" }).
 
         return o;
       }
+
+      glob(language.path + "static/global*.css", function (er, files) {
+        for (const file of files) {
+          try {
+            fs.unlinkSync(file);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      });
+
+      glob(language.path + "static/pickr*.css", function (er, files) {
+        for (const file of files) {
+          try {
+            fs.unlinkSync(file);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      });
+
+      let direction = ``;
+      if (language.lang === "ar") direction = ` dir="rtl"`;
+      let templateFile = `<!DOCTYPE html>
+      <html lang="${language.lang}"${direction}>
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      
+          %sapper.base%
+          <link rel="manifest" href="manifest.json" crossorigin="use-credentials" />
+          <link rel="sitemap" type="application/xml" href="${language.site}/sitemap.xml" />
+          <link rel="icon" type="image/png" href="favicon.png" />
+          <link rel="apple-touch-icon" href="logo-512.png" />
+          <link rel="stylesheet" href="${globalCSS}" />
+          <link rel="preload" href="${pickrCSS}" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet" />
+      
+          <meta name="author" content="Naveen CS" />
+          <meta name="theme-color" content="#44337a" />
+          <meta name="google" content="notranslate" />
+          <meta name="robots" content="index,follow" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#44337a" />
+          <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png" />
+          <meta name="msapplication-square70x70logo" content="/icons/mstile-70x70.png" />
+          <meta name="msapplication-square150x150logo" content="/icons/mstile-150x150.png" />
+          <meta name="msapplication-wide310x150logo" content="/icons/mstile-310x150.png" />
+          <meta name="msapplication-square310x310logo" content="/icons/mstile-310x310.png" />
+          <meta property="og:site_name" content="Pattern.Monster" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:site" content="@pattern_monster" />
+          <meta name="twitter:card" content="summary_large_image" />
+      
+          %sapper.styles%
+      
+          <!-- This contains the contents of the <svelte:head> component, if
+             the current page has one -->
+          %sapper.head%
+        </head>
+        <body>
+          <div id="sapper">%sapper.html%</div>
+      
+          %sapper.scripts%
+          <!-- Cloudflare Web Analytics -->
+          <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "477d2f960abf4a47b775638d604c616a"}'></script>
+          <!-- End Cloudflare Web Analytics -->
+        </body>
+      </html>`;
+
+      fs.writeFile(language.path + "src/template.html", templateFile, function (err) {
+        if (err) return console.log(err);
+      });
 
       // _lang.js
       let writeStream = fs.createWriteStream(language.path + "src/routes/_lang.js");
@@ -138,17 +231,16 @@ xlsxFile("../../../../OneDrive/Documents/Patterns.xlsm", { sheet: "Patterns" }).
       ];
 
       routesFiles.forEach((filename) => {
-        copyFile("../src/routes/" + filename, language.path + "src/routes/" + filename);
+        if (language.lang !== "en") copyFile("../src/routes/" + filename, language.path + "src/routes/" + filename);
       });
 
-      copyFile("../static/global-072421.css", language.path + "static/global-072421.css");
+      copyFile("../src/css/" + globalCSS, language.path + "static/" + globalCSS);
+      copyFile("../src/css/" + pickrCSS, language.path + "static/" + pickrCSS);
 
       function copyFile(source, dest) {
-        if (language.lang !== "en") {
-          fs.copyFile(source, dest, (err) => {
-            if (err) throw err;
-          });
-        }
+        fs.copyFile(source, dest, (err) => {
+          if (err) throw err;
+        });
       }
 
       function printObject(col) {
